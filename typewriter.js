3667e7069712dc465668862a3fdf90f3ff5184af
Bruce Lee <brucehlee@yahoo.ca>
2023-02-16 17:17:14 -0800

    Update  typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,7 @@ let sentence = "hello there from lighthouse labs";
 sentence = sentence + "\n";
 let delay = 50;
 for (const char of sentence) {
-  delay = delay + 50;
+  delay += 50;
   setTimeout(() => {
     process.stdout.write(char);
   }, delay);
